6381ab328da890d4903981c377cc87cadee1c94a
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-10-01 23:54:49 +0200

    added class 1

---
+++ 01/main.py
@@ -0,0 +1,34 @@
+# Starter
+print("Hello, World!")
+
+# Task 1 - Name
+print("Hello Joshua")
+
+# Task 2 - New line
+print("")
+print()
+
+# Task 3 - Multiple lines
+print("Hello", end="\n")
+print("How are you?", end="\n")
+print("I am good")
+print()
+print("These " + "will " + "be " + "together")
+
+# Task 4 - Multi-line string
+print("""This the start of the string
+This is the second line
+Stuff here""")
+# This is the same without pressing enter
+print("This is the start of the string\nThis is the second line\nStuff here")
+
+# Task 5 - Input data, use it to ask the user for inputs
+# input()
+# input("What is your name? ")
+
+# Task 6 - Variables
+name = input("What is your name? ")
+print("Hello", name)
+
+age = input("How old are you? ")
+print(f'You are {age} years old!')
