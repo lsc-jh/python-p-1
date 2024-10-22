b7e170120c0bde3543a41072ac4d48b8b438b0b5
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-10-22 20:09:15 +0200

    class 4

--- 03/main.py
+++ 03/main.py
@@ -21,6 +21,56 @@ last_char = text[-1]
 #if last_char != ".":
 #   print("Oh oh, you left out the punctuation!!")
 
-# TODO: Refactor this into a constant
-if last_char not in [".", "!", "?"]:
+PUNCTUATION = [".", "!", "?"]
+
+if last_char not in PUNCTUATION:
     print("Oh oh, you left out the punctuation!!")
+
+#################
+#               #
+# Loops         #
+#               #
+#################
+
+print("Numbers to 10:")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in numbers:
+    print(i)
+
+for i in range(1, 51):
+    print(i, end=", ")
+print()
+
+for c in text:
+    print(c, end="")
+
+print("\n")
+
+i = 0
+while i < 10:
+    print(i, end=", ")
+    i+=1
+print()
+
+while text:
+    print(text)
+    text = text[:-1]
+
+#           1    2      3     4
+list_1 = ["as", "sd", "s2", "asd"]
+
+print(list_1[:])
+print(list_1[2:])
+print(list_1[:2])
+print(list_1[:-1])
+print()
+print(list_1[:1])
+print(list_1[:4])
+print(list_1[1:])
+print()
+print(list_1[:-1])
+print(list_1[-1:])
+
+
+
