520a82583e3ebd7f0c573f7450d9d31943c03aff
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-01-28 21:23:36 +0100

    added better imports for section 8

--- 08/main.py
+++ 08/main.py
@@ -1,20 +1,26 @@
-import os
-import datetime
+# import os
+# import datetime
+
+# from datetime import datetime
+from datetime import datetime as dt
+
+from os import listdir, makedirs
+from os.path import join
 
 backup_dir = "backup/"
 
-files = os.listdir()
+files = listdir()
 
 for file in files:
     if not file.endswith(".txt"):
         continue
     file = file.replace(".txt", "")
 
-    current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
-    backup_folder = os.path.join(backup_dir, file)
-    os.makedirs(backup_folder, exist_ok=True)
+    current_time = dt.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
+    backup_folder = join(backup_dir, file)
+    makedirs(backup_folder, exist_ok=True)
 
-    file_path = os.path.join(backup_folder, f"{current_time[:-3]}.txt")
+    file_path = join(backup_folder, f"{current_time[:-3]}.txt")
     print(f"Saving the file as {file_path}")
     with open(file_path, "w") as f:
         with open(f"{file}.txt", "r") as f2:
