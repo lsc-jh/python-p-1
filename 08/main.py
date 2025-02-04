05bfec08934d3968afa98c1084b56813ce4f1cfb
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-02-04 19:45:53 +0100

    finished importing section

--- 08/main.py
+++ 08/main.py
@@ -1,28 +1,44 @@
 # import os
 # import datetime
-
+import os.path
 # from datetime import datetime
 from datetime import datetime as dt
 
 from os import listdir, makedirs
 from os.path import join
+import test
+import shutil
+
+
+def is_this_file_correct(file):
+    self_path = os.path.abspath(__file__)
+    return os.path.abspath(file) != self_path
+
+
+def is_folder(file):
+    return os.path.isdir(file)
+
 
 backup_dir = "backup/"
 
-files = listdir()
 
-for file in files:
-    if not file.endswith(".txt"):
-        continue
-    file = file.replace(".txt", "")
+def main():
+    files = listdir()
 
-    current_time = dt.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
-    backup_folder = join(backup_dir, file)
-    makedirs(backup_folder, exist_ok=True)
+    for file in files:
+        if not is_this_file_correct(file) or is_folder(file):
+            continue
 
-    file_path = join(backup_folder, f"{current_time[:-3]}.txt")
-    print(f"Saving the file as {file_path}")
-    with open(file_path, "w") as f:
-        with open(f"{file}.txt", "r") as f2:
-            content = f2.read()
-            f.write(content)
+        file, ext = os.path.splitext(file)
+
+        current_time = dt.now().strftime("%Y_%m_%d_%H_%M_%S.%f")
+        backup_folder = join(backup_dir, file)
+        makedirs(backup_folder, exist_ok=True)
+
+        file_path = join(backup_folder, f"{current_time[:-3]}{ext}")
+        print(f"Saving the file as {file_path}")
+        shutil.copy(f"{file}{ext}", file_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 08/test.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("Hi, I'm the test.py. Nice to meet you!")
+
+if __name__ == "__main__":
+    main()
+else:
+    print("Hi, I won't introduce myself, I'm being imported.")
